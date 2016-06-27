1a3ce330b0047bf57643996427a9d414eb738895
lariosw <lariosw@seattleu.edu>
2016-06-26 23:40:11 -0700

    final code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,32 @@
-// Place your FizzBuzz code here. 
+
+
+//initialize my start/end parameters
+var start = 1;
+var end = 20; 
+var isDivisibleByFive;
+var isDivisibleByThree;
+
+//iterate through all number between and including start and end
+for (var counter = start; counter <= end; counter+=1)
+{
+    //check that number is divisible by 5
+    isDivisibleByFive = counter%5 === 0;
+    
+    //check that number is divisible by 3
+    isDivisibleByThree = counter%3 === 0;
+    
+    if(isDivisibleByFive === true && isDivisibleByThree === true){
+        console.log("FizzBuzz");  
+    }
+    else if(isDivisibleByFive === true) {
+        console.log("Buzz");
+    }
+    else if(isDivisibleByThree === true) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(counter)
+    }
+}
+
+
